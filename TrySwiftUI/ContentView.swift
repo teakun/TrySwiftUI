87158679559ef343f2ecaf05d4eeb408e4e508f3
Yuki TAKEDA <teakun@kittengine.com>
2019-06-22 23:59:08 +0900

    Initial Commit

---
+++ TrySwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TrySwiftUI
+//
+//  Created by Yuki Takeda on 2019/06/22.
+//  Copyright © 2019 TAKEDA Yuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
